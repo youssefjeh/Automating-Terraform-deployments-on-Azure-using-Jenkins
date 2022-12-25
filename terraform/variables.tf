b64bd1e63e8c422c88d74f4d7afaf480bd5b4872
youssefjeh <youssef.jehbali@gmail.com>
2022-12-25 22:36:38 +0100

    az login

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,4 +12,18 @@ variable "env_prefix" {
 }
 variable "my_ip" {
     default = "196.74.198.37/32"
+}  
+
+variable "azure_subscription_id" {
+    default = "af6f1000-f912-4a16-978e-c46e33d5d24c"
+}
+
+variable "azure_tenant_id" {
+    default = "953309e3-79c9-437b-b3f4-b8be635b34d4"
+}
+variable "azure_client_id" {
+    default = "68c549b3-5a19-428f-b0ba-24fec9b49539"
+}
+variable "azure_client_secret" {
+    default = "Ud38Q~_JhukR6JQYgzNY8FjatdYp8~v-_353BcS7"
 }
