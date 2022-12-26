fc1bcea2e5fbc519c2abe8cff405ba776ee84544
Youssef Jehbali <85554678+youssefjeh@users.noreply.github.com>
2022-12-26 04:19:53 +0100

    azz

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,4 +8,8 @@ terraform {
 }
 provider "azurerm" {
   features {}
+  subscription_id = var.azure_subscription_id
+  tenant_id       = var.azure_tenant_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
 }
